eb7bfef5147349aebd50bd2e1176290a671ecd1f
dfrinky <dfrinky@gmail.com>
2024-06-07 17:44:07 +0200

    initial commit

---
+++ src/server.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var express = require("express"),
+	app = express(),
+    bodyParser = require("body-parser"),
+    cors = require("cors"),
+    controller = require("./controller.js");
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(express.static(__dirname + '/public'));
+
+app.route("/api/candidates/:eMail/applications")
+    .get(controller.queryApplications)
+
+app.route("/api/applications")
+    .post(controller.saveApplication)
+
+app.route("/api/:entity")
+    .get(controller.query)
+    .post(controller.save);
+app.route("/api/:entity/:id")
+    .get(controller.show)
+    .put(controller.update)
+    .delete(controller.remove);
+
+app.route("/api/:entity/:entityId/:related")
+    .get(controller.queryRelationship)
+    .post(controller.saveRelationship);
+app.route("/api/:entity/:entityId/:related/:relatedId")
+    .get(controller.showRelationship)
+    .put(controller.updateRelationship)
+    .delete(controller.removeRelationship);
+
+
+// app.listen(process.env.PORT || 3000, function() {
+//     console.log(`Server started ${process.env.PORT || 3000}`);
+// });
+
+app.listen(3000, function() {
+    console.log("Server started");
+});
